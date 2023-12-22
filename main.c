bf0a135b9ab1754229d584f006f9a23df96ec2cd
Armando Hinojosa <helloworldpy103@gmail.com>
2023-12-21 16:51:59 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    char memory[100] = {0};
+    char memory[100] = { 0 };
     char* counter_ptr = &memory;
+    FILE* file;
     char c;
 
-    while ((c = getchar()) != EOF) {
+    file = fopen("main.bl", "r");
+    if (file == NULL) {
+        printf("Cannot open file \n");
+        return 0;
+    }
+
+    while ((c = fgetc(file)) != EOF) {
         if (c == '[') {
             ++(*counter_ptr);
         }
@@ -13,17 +20,17 @@ int main() {
             --(*counter_ptr);
         }
         else if (c == '*') {
-            *counter_ptr*=2;
+            *counter_ptr *= 2;
         }
         else if (c == ',') {
-            *counter_ptr*=3;
+            *counter_ptr *= 3;
         }
         else if (c == '(') {
             ++(counter_ptr);
         }
-		else if (c == ')') {
-			--(counter_ptr);
-		}
+        else if (c == ')') {
+            --(counter_ptr);
+        }
         else if (c == '.') {
             printf("%c", *counter_ptr);
         }
@@ -31,6 +38,6 @@ int main() {
             memset(memory, 0, sizeof(memory));
         }
     }
-
+    fclose(file);
     return 0;
 }
